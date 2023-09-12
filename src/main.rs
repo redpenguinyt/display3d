0870e395ac2669f9d4a44c82abf2c048641797c9
Red Penguin <timmyterskov@gmail.com>
2023-09-12 16:26:21 +0100

    now using `fps_gameloop!` macro for main loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use gemini_engine::elements::view::{ColChar, View, Wrapping};
 use gemini_engine::elements3d::{DisplayMode, Mesh3D, Transform3D, Vec3D, Viewport};
-use gemini_engine::gameloop;
+use gemini_engine::{fps_gameloop, gameloop};
 use obj_view::obj_to_mesh3ds;
 use tobj;
 
@@ -13,7 +13,6 @@ const FOV: f64 = 95.0;
 
 fn main() {
     let mut view = View::new(WIDTH, HEIGHT, ColChar::BACKGROUND);
-    let mut frame_skip = false;
 
     let mut viewport = Viewport::new(
         Transform3D::new_tr(Vec3D::new(0.0, -0.7, 2.2), Vec3D::new(-0.3, 0.0, 0.0)),
@@ -29,41 +28,40 @@ fn main() {
 
     let mesh3d_models: Vec<Mesh3D> = obj_to_mesh3ds(models, materials);
 
-    loop {
-        let now_blitting = gameloop::Instant::now();
-        let now = gameloop::Instant::now();
-        let mut elapsed_blitting = None;
-        let mut now_rendering = None;
-        view.clear();
+    let mut elapsed_blitting;
+    let mut elapsed_rendering;
+    fps_gameloop!(
+        { // Logic
+            view.clear();
+            viewport.transform.rotation.y += 0.05;
+            elapsed_blitting = None;
+            elapsed_rendering = None;
+        },
+        { // Rendering
+            let now_blitting = gameloop::Instant::now();
+            view.blit(
+                &viewport.render(mesh3d_models.iter().collect(), DisplayMode::Solid),
+                Wrapping::Ignore,
+            );
+            elapsed_blitting = Some(now_blitting.elapsed());
 
-        viewport.transform.rotation.y += 0.05;
-
-        match frame_skip {
-            true => frame_skip = false,
-            false => {
-                view.blit(
-                    &viewport.render(mesh3d_models.iter().collect(), DisplayMode::Solid),
-                    Wrapping::Ignore,
-                );
-
-                elapsed_blitting = Some(now_blitting.elapsed());
-                now_rendering = Some(gameloop::Instant::now());
-                view.display_render().unwrap();
-            }
-        }
-
-        let elapsed = now.elapsed();
-        let elapsed_rendering = now_rendering.unwrap_or(gameloop::Instant::now()).elapsed();
-        println!(
+            let now_rendering = gameloop::Instant::now();
+            view.display_render().unwrap();
+            elapsed_rendering = Some(now_rendering.elapsed());
+        },
+        FPS,
+        |elapsed: gameloop::Duration, frame_skip| { // Timing info
+            println!(
             "Elapsed - Blitting: {:.2?}µs, Rendering: {:.2?}µs, Total: {:.2?}µs | Frame skip: {}",
             elapsed_blitting
                 .unwrap_or(gameloop::Duration::default())
                 .as_micros(),
-            elapsed_rendering.as_micros(),
+            elapsed_rendering
+                .unwrap_or(gameloop::Duration::default())
+                .as_micros(),
             elapsed.as_micros(),
             frame_skip
         );
-
-        frame_skip = gameloop::sleep_fps(FPS, Some(elapsed));
-    }
+        }
+    );
 }
