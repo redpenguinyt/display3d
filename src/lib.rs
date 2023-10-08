abd733e5f29b3f15f42d4b4de1b3cc849fb50594
Red Penguin <timmyterskov@gmail.com>
2023-10-08 18:31:40 +0100

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,74 +1,74 @@
+mod obj_to_mesh3d;
+use std::time::{Duration, Instant};
+
 use gemini_engine::{
-    elements::view::ColChar,
-    elements3d::{Face, Mesh3D, Transform3D, Vec3D},
+    elements::{view::Wrapping, View},
+    elements3d::{DisplayMode, Mesh3D, Transform3D, Viewport},
+    gameloop::{MainLoopRoot, sleep_fps},
 };
-use tobj::{Material, Model};
+pub use obj_to_mesh3d::{get_obj_from_file, obj_to_mesh3ds};
 
-pub fn get_mateial_as_col_char(materials: &[Material], material_id: Option<usize>) -> ColChar {
-    let colour_rgb = match material_id {
-        Some(material_id) => materials[material_id].diffuse.unwrap(),
-        None => [1.0, 0.0, 1.0],
-    };
-
-    ColChar::SOLID.with_rgb(
-        (colour_rgb[0] * 255.0) as u8,
-        (colour_rgb[1] * 255.0) as u8,
-        (colour_rgb[2] * 255.0) as u8,
-    )
+pub struct Root {
+    view: View,
+    viewport: Viewport,
+    models: Vec<Mesh3D>,
+    // Timing stats
+    elapsed_blitting: Duration,
+    elapsed_rendering: Duration,
 }
 
-pub fn model_to_mesh3d(model: &Model, materials: &[Material]) -> Mesh3D {
-    let mesh = &model.mesh;
-
-    // let all_texcoords: Vec<Vector2<f64>> = mesh.texcoords.chunks(2).map(|k| Vector2::new(k[0] as f64, k[1] as f64)).collect();
-    // let indexed_texcoords: Vec<Vector2<f64>> = mesh.texcoord_indices.iter().map(|i| all_texcoords[*i as usize]).collect();
-
-    let vertices = mesh
-        .positions
-        .chunks(3)
-        .map(|v| Vec3D::new(v[0].into(), v[1].into(), v[2].into()))
-        .collect();
-
-    let faces: Vec<Face> = match mesh.face_arities.len() {
-        // If face_arities is empty (triangulate is on or mesh consists of triangles only)
-        0 => mesh
-            .indices
-            .chunks(3)
-            .map(|v| {
-                let v_indices = v.iter().map(|i| *i as usize).collect();
-                Face::new(
-                    v_indices,
-                    get_mateial_as_col_char(materials, mesh.material_id),
-                )
-            })
-            .collect(),
-        // Otherwise
-        _ => {
-            let mut next_face = 0;
-            (0..mesh.face_arities.len())
-                .map(|f| {
-                    let end = next_face + mesh.face_arities[f] as usize;
-                    let face_indices = mesh.indices[next_face..end]
-                        .iter()
-                        .map(|i| *i as usize)
-                        .rev()
-                        .collect();
-
-                    let material = get_mateial_as_col_char(materials, mesh.material_id);
-
-                    next_face = end;
-                    Face::new(face_indices, material)
-                })
-                .collect()
+impl Root {
+    pub fn new(
+        canvas: View,
+        fov: f64,
+        initial_viewport_transform: Transform3D,
+        models: Vec<Mesh3D>,
+    ) -> Root {
+        let viewport_center = canvas.center();
+        Root {
+            view: canvas,
+            viewport: Viewport::new(initial_viewport_transform, fov, viewport_center),
+            models,
+            elapsed_blitting: Duration::ZERO,
+            elapsed_rendering: Duration::ZERO,
         }
-    };
-
-    Mesh3D::new(Transform3D::DEFAULT, vertices, faces)
+    }
 }
 
-pub fn obj_to_mesh3ds(models: Vec<Model>, materials: Vec<Material>) -> Vec<Mesh3D> {
-    models
-        .iter()
-        .map(|model| model_to_mesh3d(model, &materials))
-        .collect()
+impl MainLoopRoot for Root {
+    type InputDataType = u8;
+    fn frame(&mut self, _input_data: Option<Self::InputDataType>) {
+        self.viewport.transform.rotation.y += 0.05;
+    }
+
+    fn render_frame(&mut self) {
+        self.view.clear();
+        let now = Instant::now();
+        self.view.blit(
+            &self
+                .viewport
+                .render(self.models.iter().collect(), DisplayMode::Solid),
+            Wrapping::Ignore,
+        );
+        self.elapsed_blitting = now.elapsed();
+        let now = Instant::now();
+        self.view.display_render().unwrap();
+        self.elapsed_rendering = now.elapsed();
+    }
+
+    fn sleep_and_get_input_data(
+        &self,
+        fps: f32,
+        elapsed: Duration,
+    ) -> (bool, Option<Self::InputDataType>) {
+        // Hijack the sleep function to print elapsed times before falling back to default sleep function
+        println!(
+            "Elapsed - Blitting: {:.2?}µs, Rendering: {:.2?}µs, Total: {:.2?}µs",
+            self.elapsed_blitting.as_micros(),
+            self.elapsed_rendering.as_micros(),
+            elapsed.as_micros()
+        );
+
+        (sleep_fps(fps, Some(elapsed)), None)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
 
-use gemini_engine::elements::view::{ColChar, View, Wrapping};
-use gemini_engine::elements3d::{DisplayMode, Mesh3D, Transform3D, Vec3D, Viewport};
-use gemini_engine::{fps_gameloop, gameloop};
-use obj_view::obj_to_mesh3ds;
+use gemini_engine::elements::view::{ColChar, View};
+use gemini_engine::elements3d::{Mesh3D, Transform3D, Vec3D};
+use gemini_engine::gameloop::MainLoopRoot;
+use obj_view::Root;
 
 // const OBJ_FILEPATH: &str = "obj-view/resources/ren.obj";
 // const MTL_FILEPATH: &str = "obj-view/model.mtl";
@@ -15,59 +15,17 @@ const FOV: f64 = 95.0;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let obj_filepath = &args[1];
-    let mut view = View::new(WIDTH, HEIGHT, ColChar::BACKGROUND);
 
-    let mut viewport = Viewport::new(
-        Transform3D::new_tr(Vec3D::new(0.0, -0.7, 2.2), Vec3D::new(-0.3, 0.0, 0.0)),
+    let (models, materials) = obj_view::get_obj_from_file(obj_filepath);
+    let mesh3d_models: Vec<Mesh3D> = obj_view::obj_to_mesh3ds(models, materials);
+
+    let mut root = Root::new(
+        View::new(WIDTH, HEIGHT, ColChar::BACKGROUND),
         FOV,
-        view.center(),
+        Transform3D::new_tr(Vec3D::new(0.0, -0.7, 2.2), Vec3D::new(-0.3, 0.0, 0.0)),
+        mesh3d_models,
     );
-    viewport.character_width_multiplier = 2.2;
 
-    let load_options = tobj::LoadOptions::default();
-    let (models, materials) =
-        tobj::load_obj(obj_filepath, &load_options).expect("Failed to OBJ load file");
-    let materials = materials.unwrap_or(vec![]); // TODO: fallback to MTL_FILEPATH
 
-    let mesh3d_models: Vec<Mesh3D> = obj_to_mesh3ds(models, materials);
-
-    let mut elapsed_blitting;
-    let mut elapsed_rendering;
-    fps_gameloop!(
-        {
-            // Logic
-            viewport.transform.rotation.y += 0.05;
-            elapsed_blitting = None;
-            elapsed_rendering = None;
-        },
-        {
-            // Rendering
-            view.clear();
-            let now_blitting = gameloop::Instant::now();
-            view.blit(
-                &viewport.render(mesh3d_models.iter().collect(), DisplayMode::Solid),
-                Wrapping::Ignore,
-            );
-            elapsed_blitting = Some(now_blitting.elapsed());
-
-            let now_rendering = gameloop::Instant::now();
-            view.display_render().unwrap();
-            elapsed_rendering = Some(now_rendering.elapsed());
-        },
-        FPS,
-        |elapsed: gameloop::Duration, frame_skip| {
-            // Timing info
-            println!(
-            "Elapsed - Blitting: {:.2?}µs, Rendering: {:.2?}µs, Total: {:.2?}µs | Frame skip: {}",
-            elapsed_blitting
-                .unwrap_or(gameloop::Duration::default())
-                .as_micros(),
-            elapsed_rendering
-                .unwrap_or(gameloop::Duration::default())
-                .as_micros(),
-            elapsed.as_micros(),
-            frame_skip
-        );
-        }
-    );
+    root.main_loop(FPS);
 }
